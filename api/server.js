82f2c9b41b31bda74ec926a5a63380614efa238f
Terra SARR <samba.co93@gmail.com>
2023-11-04 23:09:21 +0000

    create signup & api route

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -15,11 +16,13 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
-// Port
-const port = process.env.PORT || 3000;
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server is running on ${port}...`);
+
+app.listen(3000, () => {
+  console.log('Server is running on 3000...');
 });
 
 app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
+
